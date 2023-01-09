ea1c881a2450e0f9c27863160264512072d37c38
Andrey Vladimirov <vappiter@yandex.ru>
2023-01-09 22:02:38 +0300

    To continue

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,41 +1,58 @@
 from rest_framework import serializers
-
+from .models import Product, Stock, StockProduct
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
-
-
+    class Meta:
+        model = Product
+        fields = ['id', 'title','description']
+        
+ 
 class ProductPositionSerializer(serializers.ModelSerializer):
+         
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
     # настройте сериализатор для позиции продукта на складе
-    pass
+    
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+        
     # настройте сериализатор для склада
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
-
+        for position in positions:
+          position["stock"] = stock
+          StockProduct.objects.create(**position)
+        return stock    
+        
+        # создаем склад по его параметрам
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
 
-        return stock
+       
 
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
+        print(positions)
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-
+        # print (stock)
+        for position in positions:
+          print (position)
+        #   if position[0][1] == product.pk:
+        #     print (position)  
+          StockProduct.objects.update(stock_id=stock.pk,**position)
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -80,9 +80,11 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_stocks_products',
+        'NAME': 'db_django',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '123456'
     }
 }
 
